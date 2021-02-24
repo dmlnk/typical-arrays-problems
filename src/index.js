b656c03abd40513fbbe205e8088d3fa2a777adf8
Daniel Miller <daniel.miller.prs@gmail.com>
2021-02-24 22:34:58 +0100

    problem resolved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  let total = 0;
+  for (i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return (total / array.length);
 }
